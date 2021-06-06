d50d2b84a71c3ebc830edb3eb3a5e283e511e7db
esfox <osairis100@gmail.com>
2021-06-06 15:33:07 +0800

    Cleanup and add tachyons

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+
+import 'tachyons/css/tachyons.min.css';
+import './styles/index.css';
+import './styles/reset.css';
+
+import { App } from './App';
+
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
